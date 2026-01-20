fb9b364e2e788db5c65b369c1c6856bf7df59e73
Guilherme Monni <gmonni20@gmail.com>
2026-01-20 20:53:07 -0300

    fix cadastrado.php

--- cadastrado.php
+++ cadastrado.php
@@ -1,23 +1,21 @@
-<?php
-
-    if(!isset($_SESSION)){
-        session_start();
-    }
-
-    if(!isset($_SESSION['id'])){
-        die ("Cadastro realizado com sucesso! <p><a href=\"index.php\">Entrar</a></p>");
-    }
-
-?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="favicon_io/favicon.ico" type="image/x-icon">
     <title>Cadastro</title>
 </head>
+
 <body>
-    
+    <div class="container-sucess">
+        <div class="msg-sucess">
+            <h1>✅ Cadastro Realizado com Sucesso!</h1>
+            <p>Sua conta foi criada. Agora você pode fazer login.</p>
+            <a href="index.php" class="btn-entrar">Fazer Login</a>
+        </div>
+    </div>
 </body>
+
 </html>
